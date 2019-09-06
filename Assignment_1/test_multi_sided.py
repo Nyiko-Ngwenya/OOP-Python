1d479fde31677fc3e3c0bd7a37a417347e912108
Nyiko-Ngwenya <nyikongwenya@gmail.com>
2019-09-06 15:54:52 +0200

    Added Folders

--- Assignment_1/test_multi_sided.py
+++ Assignment_1/test_multi_sided.py
@@ -6,17 +6,12 @@ def test_addition():
     assert dice_instance.roll() == 3
     assert dice_instance.roll() == 1
 
-def test_get_value():
+def test_set_and_get_value():
     dice_instance = MultiSidedDie(8)
-    assert dice_instance.set_value(8) 
+    dice_instance.set_value(8) 
     assert dice_instance.get_value() == 8
-    assert dice_instance.set_value(7) 
+    dice_instance.set_value(7) 
     assert dice_instance.get_value() == 7
-    assert dice_instance.set_value(6) 
+    dice_instance.set_value(6) 
     assert dice_instance.get_value() == 6
 
-# def test_set_value():
-#     dice_instance = MultiSidedDie(10)
-#     assert dice_instance.set_value(3) == 6
-#     assert dice_instance.get_value() == 9
-#     assert dice_instance.roll(3, 3,3,3) == 12
