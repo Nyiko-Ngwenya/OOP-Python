c6402819ccc79dadfdb85bd86e1931aaa4fd6285
Nyiko-Ngwenya <nyikongwenya@gmail.com>
2019-09-10 16:18:03 +0200

    Latest additions to the score function

--- Assignment_2/dice_poker.py
+++ Assignment_2/dice_poker.py
@@ -5,10 +5,27 @@ class Poker:
     rolls = 2
     dice_rolls = []
     def __init__(self):
-        self.money -= 10
-        for i in range(5):
-            i = random.randint(1,6)
-            self.dice_rolls.append(i)
+        while self.money>0:
+            print(f'Your money: {self.money}')
+            move = input('What is your move :')
+            if move == 'play':
+                self.money -= 10 
+                for i in range(5):
+                    i = random.randint(1,6)
+                    self.dice_rolls.append(i)
+                print(f'Your current dice rolls : {self.dice_rolls}')
+            if move == 'quit':
+                break
+            elif move == 'score':
+                self.score()
+            elif move == 'roll':
+                roll_index = input('Which is the dice you want to reroll:')
+            else:
+                print('Please try again cant recognise command.')
+
+
+
+
     
     def roll(self,dice_change):
         self.dice_rolls[dice_change] = random.randint(1,6)
@@ -17,6 +34,8 @@ class Poker:
         return self.dice_rolls
 
     def reset_round(self):
+        pass
+    def start(self):
         pass
 
     def score(self):
@@ -51,9 +70,4 @@ class Poker:
             return 'Nothing matched'
 
 poker_instance = Poker()
-print(poker_instance.score())
-print(poker_instance.value())
-print(poker_instance.roll(0))
-print(poker_instance.value())
-print(poker_instance.score())
 
